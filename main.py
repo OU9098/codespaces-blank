73bb3a2fcd828367707d66cb4d98b394e688d972
OU9098 <onyiugbaja58@gmail.com>
2025-01-04 17:01:04 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-place_holder_1='1'
-place_holder_2='2'
-place_holder_3='3'
-exit_conversation_message='4'
 
 print("Goodmorning user, welcome to this interface! today I am your chatbot")
 name=input("Hello, what is your name?")
@@ -10,25 +6,27 @@ print("Oh to have an an acutual age, despite my capbabilities I can't even tell 
 
 print()
 
-print("Please select one of the five presented choices below:")
-menu_options=['place holder 1','place holder 2','place holder 3','exit conversation message']
-for word in menu_options:
-    print ('-' + word)
+print("Please select one of the four presented choices below:")
+print("------------")
+print(" 1. Place holder 1")
+print(" 2. Place holder 2")
+print(" 3. Place holder 3")
+print(" 4. Exit conversation message")
 
-menu_choice=input("Enter a number you wish to use:")
+print()
+menu_choice=input("Please select a number of your choice (1 2 3 or 4)")
 
 if menu_choice=='1':
     print("Here is the mail you recieved")
 elif menu_choice=='2':
     print("This is a safe space")
 elif menu_choice =='3':
-    print("Help is on the way")
+    print("I am gathering infomation")
+    
 elif menu_choice =='4':
     print(f"Goodbye {name}, thank you for choosing this interface")
 
 else:
-    print("Please choose a number between 1 through 4")
-
-
+    print("Error: This is invalid, please choose a number containing 1 to 4 ")
 
 
